a1d91f705ba917083f42cd1c467318c75e6a3039
Yudistira <kizoukungaming@gmail.com>
2023-04-03 10:53:41 +0700

    working dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,42 @@
 @extends('components.layouts.app')
 
 @section('content')
+    <div class="space-y-4">
+        <div class="grid grid-cols-5 gap-5">
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+        </div>
+        <div class="grid grid-cols-3 gap-5">
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500 col-span-2">
+                <h1>ads</h1>
+            </div>
+            <div class="bg-blue-500">
+                <h1>ads</h1>
+            </div>
+        </div>
+    </div>
     <h1>Stock List</h1>
     @foreach($stocks as $stock)
         <div>
