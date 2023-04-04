2f75ed2ad623c240157dd2cc52ec735fa7c4ccb6
Yudistira <kizoukungaming@gmail.com>
2023-04-04 20:11:25 +0700

    shouldn't be able to edit and working on deletion

--- resources/views/admin/OpenOrders/show.blade.php
+++ resources/views/admin/OpenOrders/show.blade.php
@@ -59,11 +59,7 @@
                         {{ $open->order_price }}
                     </td>
                     <td class="p-3 text-center space-y-2    ">
-                        <a href="/admin/users/{{ $open->id }}" class="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-2 py-1 leading-5 text-sm rounded border-gray-300 bg-white text-gray-800 shadow-sm hover:text-gray-800 hover:bg-gray-100 hover:border-gray-300 hover:shadow focus:ring focus:ring-gray-500 focus:ring-opacity-25 active:bg-white active:border-white active:shadow-none">
-                            <svg class="hi-solid hi-pencil inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/></svg>
-                            <span class="hidden sm:inline">Edit</span>
-                        </a>
-                        <form id="deleteForm" method="POST" action="/admin/stocks/{{ $open->id }}">
+                        <form id="deleteForm" method="POST" action="/admin/open-orders/{{ $open->id }}">
                             @csrf
                             @method("DELETE")
                             <button type="submit" class="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-2 py-1 leading-5 text-sm rounded border-gray-300 bg-white text-gray-800 shadow-sm hover:text-gray-800 hover:bg-gray-100 hover:border-gray-300 hover:shadow focus:ring focus:ring-gray-500 focus:ring-opacity-25 active:bg-white active:border-white active:shadow-none">
