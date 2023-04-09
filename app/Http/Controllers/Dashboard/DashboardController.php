fbb1c3dbce3d7f073b8a08509aa2bc77d4173ed6
Yudistira <kizoukungaming@gmail.com>
2023-04-09 13:28:43 +0700

    fix

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
     public function index() {
         $result = DB::select("SELECT * FROM stocks;");
         $user_watchList = DB::select("SELECT wl.*, stocks.name, stocks.image_uri, (select order_price FROM orders WHERE stock_symbol = stocks.symbol  ORDER BY orders.created_at DESC LIMIT 1) as price FROM watch_lists wl JOIN stocks ON stocks.symbol = wl.stock_symbol WHERE user_id = ?", [auth()->id()]);
-        $user_stock_amounts = $this->getUserStocksAndAmounts(auth()->id());
+        $user_stock_amounts = $this->getUserStocksAndAmounts(auth()->id(), false);
         return view("dashboard", ["stocks" => $result, "user_stock_amounts" => $user_stock_amounts, "user_watchList" => $user_watchList]);
     }
 
@@ -34,17 +34,15 @@ class DashboardController extends Controller
         return $result[0]->amount ?? 0;
     }
 
-    public function getUserStocksAndAmounts($userId): array
+    public function getUserStocksAndAmounts($userId, $calculateOpenOrder = true): array
     {
         // this query should be used in line 45 and 46 but because its not currently working
-//        IF(orders.order_type = 'BUY', orders.filled_quantity, 0)
-//                    ".($calculateOpenOrder ?"- IF(orders.order_type = 'SELL', IFNULL(open_orders.total_order_quantity, 0) + orders.filled_quantity, 0)." : "")."
+
         return DB::select("SELECT stocks.*,
             COALESCE((
                 SELECT SUM(
-                    IF(orders.order_type = 'BUY', orders.filled_quantity, 0) -
-                    IF(orders.order_type = 'SELL', IFNULL(open_orders.total_order_quantity, 0) + orders.filled_quantity, 0)
-                )
+                    IF(orders.order_type = 'BUY', orders.filled_quantity, 0)
+                    ".($calculateOpenOrder ? "- IF(orders.order_type = 'SELL', IFNULL(open_orders.total_order_quantity, 0) + orders.filled_quantity, 0)" : "")."                )
                 FROM orders
                 LEFT JOIN (
                     SELECT order_id, SUM(order_quantity) AS total_order_quantity
